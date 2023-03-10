a5b3b5e68effbdbbf0f755a416036e227379a6b3
stanken_a <artyemst3@mail.ru>
2023-03-10 13:35:35 +0300

    feat: add search function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 
 import PostList from './PostList';
 import DropdownMenu from './DropdownMenu';
@@ -11,6 +11,28 @@ export default function Main() {
 
   const [posts, setPosts] = useState(initialPosts);
   const [isPopupOpened, setIsPopupOpened] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedSort, setSelectedSort] = useState('');
+
+  // Колбэк будет вызван только в том случае, если какой-либо депс поменяет своё значение
+  // В нашем случае необходимо следить за изменением алгоритма сортировки и массива постов
+  const sortedPosts = useMemo(() => {
+
+    // Проверяем, был ли сортирован массив
+    // P.S. Сделано это потому, что изначальный стейт selectedSort равен пустой строке.
+    // При попытке передать его функции localeCompare она не отработает
+
+    console.log('Отраб')
+
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort.toLowerCase()].localeCompare(b[selectedSort.toLowerCase()]))
+    }
+    return posts;
+  }, [selectedSort, posts])
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  }, [searchQuery, sortedPosts])
 
   function addPost(newPost) {
     setPosts([...posts, newPost]);
@@ -30,19 +52,34 @@ export default function Main() {
   }
 
   function sortPosts(sort) {
-    setPosts([...posts].sort((a, b) => a[sort.toLowerCase()].localeCompare(b[sort.toLowerCase()])))
+    setSelectedSort(sort);
   }
 
   return (
     <main className="content">
       <section className="posts">
         <h2 className="posts__title">Make your story.</h2>
-        <button className="posts__button button" onClick={openPopup}>Create new post</button>
-        <DropdownMenu onSort={sortPosts} />
-        {posts.length !== 0
-          ? <PostList posts={posts} onPostRemove={removePost} />
-          : <h2 className="posts__subtitle">{`No posts? ( ͠° ͟ʖ ͡°)`}</h2>
-        }
+        <button type="button" className="posts__button button" onClick={openPopup}>Create new post</button>
+        <div className="posts__search-container">
+          <DropdownMenu onSort={sortPosts} />
+          <label htmlFor="search" className="posts__search-label">Search</label>
+          <div className="posts__search-input">
+            <input
+              name="search"
+              type="text"
+              className="posts__search"
+              placeholder="Search..."
+              value={searchQuery}
+              onChange={evt => setSearchQuery(evt.target.value)}
+            />
+          </div>
+        </div>
+        <div className="posts__container">
+          {sortedAndSearchedPosts.length !== 0
+            ? <PostList posts={sortedAndSearchedPosts} onPostRemove={removePost} />
+            : <h2 className="posts__subtitle">{`No posts? ( ͠° ͟ʖ ͡°)`}</h2>
+          }
+        </div>
       </section>
       <Popup isOpened={isPopupOpened} onClose={closePopup}>
         <PostForm onAddPost={addPost} />
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,10 +3,10 @@ import Post from './Post';
 
 export default function PostList({ posts, onPostRemove }) {
   return (
-    <div className="posts__container">
+    <>
       {posts.map((post, index) => (
         <Post number={index + 1} key={post.id} post={post} onPostRemove={onPostRemove} />
       ))}
-    </div>
+    </>
   )
 }
