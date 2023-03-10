0587d2a0bbc61ca6fb9242ccb7408473412f96a0
stanken_a <artyemst3@mail.ru>
2023-03-10 14:35:07 +0300

    feat: add animations for post creation/deletion

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import Post from './Post';
 
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 export default function PostList({ posts, onPostRemove }) {
 
   return (
     <div className="posts__container">
       {posts.length !== 0
-        ? posts.map((post, index) => (
-          <Post number={index + 1} key={post.id} post={post} onPostRemove={onPostRemove} />
-        ))
+        ? <TransitionGroup>
+          {posts.map((post, index) => (
+            <CSSTransition
+              key={post.id}
+              timeout={800}
+              classNames="animation"
+            >
+              <Post number={index + 1} post={post} onPostRemove={onPostRemove} />
+            </CSSTransition>
+
+          ))}
+        </TransitionGroup>
         : <h2 className="posts__subtitle">{`No posts? ( ͠° ͟ʖ ͡°)`}</h2>
       }
     </div>
